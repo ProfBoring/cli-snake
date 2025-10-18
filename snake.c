c89208e29499d958f969ea4c8c6c3e4abeb6bfe2
Athul <athul28907@gmail.com>
2025-10-18 23:04:37 +0530

    feat: added boundary and moving snake

---
+++ snake.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+int width = 30;
+int height = 30;
+int x_snake = 10;   // initial snake X
+int y_snake = 5;    // initial snake Y
+char dir = 'd';     // initial direction: 'w','a','s','d'
+int isRunning = 1;
+void drawGame(int width, int height)
+{
+//creating top boundary
+ for(int i=0;i<width+2;i++){
+    printf("[]");
+ }
+ printf("\n");
+ //creating snake and left and right boundaries
+ for(int y=0;y<height;y++){
+    printf("[]");
+    for(int x=0;x<width;x++){
+        if(x==x_snake && y==y_snake)
+            printf("O ");
+        else
+            printf("  ");
+    }
+    printf("[]\n");
+ }
+ //creating bottom boundary
+ for(int i=0;i<width+2;i++){
+    printf("[]");}
+    printf("\n");}
+
+void gameInput(){
+    if( _kbhit()){
+        char key=_getch();
+        switch(key){
+        case 'w': dir = 'w'; break;
+        case 'a': dir = 'a'; break;
+        case 's': dir = 's'; break;
+        case 'd': dir = 'd'; break;
+        }
+    }
+}
+void logic() {
+    switch (dir) {
+        case 'w': y_snake--; break;
+        case 's': y_snake++; break;
+        case 'a': x_snake--; break;
+        case 'd': x_snake++; break;
+    }
+
+    // collision with walls → stop game
+    if (x_snake < 0 || x_snake >= width || y_snake < 0 || y_snake >= height)
+        isRunning = 0;
+}
+
+int main(){
+    while (isRunning) {
+        drawGame(width, height);
+        gameInput();
+        logic();
+        Sleep(500);
+        system("cls"); // Clear screen for next frame
+    }
+}
+
+
+
